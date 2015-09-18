d4233e594aa7a91e88a84983136f6a53c4d8cf94
Ooppa <OpaPosti@hotmail.com>
2015-09-18 17:31:50 +0300

    SQL updates and DOC

--- docs/add_test_data.sql
+++ docs/add_test_data.sql
@@ -8,27 +8,29 @@ COMMIT;
 
 START TRANSACTION;
 USE `oobbit`;
-INSERT INTO `oobbit`.`users` (`id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (22, 'admin', 'admin@ooppa.fi', 'admin', 100, 'NOW()');
-INSERT INTO `oobbit`.`users` (`id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (23, 'moderator', 'moderator@ooppa.fi', 'moderator', 10, 'NOW()');
-INSERT INTO `oobbit`.`users` (`id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (24, 'user', 'user@ooppa.fi', 'user', 1, 'NOW()');
-INSERT INTO `oobbit`.`users` (`id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (25, 'banned', 'troll@ooppa.fi', 'troll', 0, 'NOW()');
-INSERT INTO `oobbit`.`users` (`id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (26, 'user2', 'user2@ooppa.fi', 'user2', 1, 'NOW()');
+INSERT INTO `oobbit`.`users` (`user_id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (22, 'admin', 'admin@ooppa.fi', 'admin', 100, '2015-01-01 00:00:00');
+INSERT INTO `oobbit`.`users` (`user_id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (23, 'moderator', 'moderator@ooppa.fi', 'moderator', 10, '2015-01-01 00:00:00');
+INSERT INTO `oobbit`.`users` (`user_id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (24, 'user', 'user@ooppa.fi', 'user', 1, '2015-01-01 00:00:00');
+INSERT INTO `oobbit`.`users` (`user_id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (25, 'banned', 'troll@ooppa.fi', 'troll', 0, '2015-01-01 00:00:00');
+INSERT INTO `oobbit`.`users` (`user_id`, `username`, `email`, `password`, `access_level`, `create_time`) VALUES (26, 'user2', 'user2@ooppa.fi', 'user2', 1, '2015-01-01 00:00:00');
 COMMIT;
 
 START TRANSACTION;
 USE `oobbit`;
-INSERT INTO `oobbit`.`categories` (`id`, `title`, `description`) VALUES (1, 'Cat pictures', 'Pictures of cats!');
-INSERT INTO `oobbit`.`categories` (`id`, `title`, `description`) VALUES (2, 'Funny pictures', 'Pictures of funny!');
+INSERT INTO `oobbit`.`categories` (`category_id`, `title`, `description`) VALUES ('v', 'Videos', 'Category - Videos');
+INSERT INTO `oobbit`.`categories` (`category_id`, `title`, `description`) VALUES ('p', 'Pictures', 'Category - Pictures');
+INSERT INTO `oobbit`.`categories` (`category_id`, `title`, `description`) VALUES ('m', 'Music', 'Category - Music');
+INSERT INTO `oobbit`.`categories` (`category_id`, `title`, `description`) VALUES ('d', 'Discussion', 'Category - Discussion');
 COMMIT;
 
 START TRANSACTION;
 USE `oobbit`;
-INSERT INTO `oobbit`.`links` (`id`, `title`, `content`, `link`, `category`, `creator`, `create_time`, `edit_time`) VALUES (1, 'Pssst... Hey, You Guys Wanna Buy a Cat?', 'Cat in a box', 'https://i.imgur.com/6LCNnDm.jpg', 1, 24, 'NOW()', NULL);
-INSERT INTO `oobbit`.`links` (`id`, `title`, `content`, `link`, `category`, `creator`, `create_time`, `edit_time`) VALUES (2, 'Heart nose', 'A cat has a LOVELY nose, haha. Get it?', 'https://i.imgur.com/I4vJzFY.jpg', 1, 26, 'NOW()', NULL);
+INSERT INTO `oobbit`.`links` (`link_id`, `title`, `content`, `link`, `category`, `creator`, `create_time`, `edit_time`) VALUES (1, 'Pssst... Hey, You Guys Wanna Buy a Cat?', 'Cat in a box', 'https://i.imgur.com/6LCNnDm.jpg', 'p', 24, '2015-01-01 00:00:00', NULL);
+INSERT INTO `oobbit`.`links` (`link_id`, `title`, `content`, `link`, `category`, `creator`, `create_time`, `edit_time`) VALUES (2, 'Heart nose', 'A cat has a LOVELY nose, haha. Get it?', 'https://i.imgur.com/I4vJzFY.jpg', 'p', 26, '2015-01-01 00:00:00', NULL);
 COMMIT;
 
 START TRANSACTION;
 USE `oobbit`;
-INSERT INTO `oobbit`.`comments` (`id`, `link_id`, `creator`, `content`, `create_time`) VALUES (1, 1, 22, 'This link is so good I need to ban you!', 'NOW()');
-INSERT INTO `oobbit`.`comments` (`id`, `link_id`, `creator`, `content`, `create_time`) VALUES (2, 2, 23, 'Nice link, moderator approves!', 'NOW()');
+INSERT INTO `oobbit`.`comments` (`comment_id`, `link_id`, `creator`, `content`, `create_time`) VALUES (1, 1, 22, 'This link is so good I need to ban you!', '2015-01-01 00:00:00');
+INSERT INTO `oobbit`.`comments` (`comment_id`, `link_id`, `creator`, `content`, `create_time`) VALUES (2, 2, 23, 'Nice link, moderator approves!', '2015-01-01 00:00:00');
 COMMIT;
--- docs/create_tables.sql
+++ docs/create_tables.sql
@@ -3,20 +3,20 @@ USE `oobbit` ;
 
 CREATE TABLE IF NOT EXISTS `oobbit`.`access_levels` (
   `level` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL DEFAULT '',
-  `description` VARCHAR(255) NOT NULL DEFAULT '',
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`level`))
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `oobbit`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `access_level` INT NOT NULL DEFAULT 1,
-  `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `access_level` INT NOT NULL,
+  `create_time` DATETIME NOT NULL,
   INDEX `access_level_idx` (`access_level` ASC),
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`user_id`),
   CONSTRAINT `user_access_level`
     FOREIGN KEY (`access_level`)
     REFERENCES `oobbit`.`access_levels` (`level`)
@@ -25,32 +25,32 @@ CREATE TABLE IF NOT EXISTS `oobbit`.`users` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `oobbit`.`categories` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `category_id` VARCHAR(3) NOT NULL,
   `title` VARCHAR(45) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`category_id`))
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `oobbit`.`links` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `link_id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `content` MEDIUMTEXT NULL,
   `link` VARCHAR(255) NOT NULL,
-  `category` INT NOT NULL,
+  `category` VARCHAR(3) NOT NULL,
   `creator` INT NOT NULL,
-  `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `edit_time` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  `create_time` DATETIME NOT NULL,
+  `edit_time` DATETIME NULL,
+  PRIMARY KEY (`link_id`),
   INDEX `creator_idx` (`creator` ASC),
   INDEX `catgory_idx` (`category` ASC),
   CONSTRAINT `link_creator_id`
     FOREIGN KEY (`creator`)
-    REFERENCES `oobbit`.`users` (`id`)
+    REFERENCES `oobbit`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `link_catgory_id`
     FOREIGN KEY (`category`)
-    REFERENCES `oobbit`.`categories` (`id`)
+    REFERENCES `oobbit`.`categories` (`category_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -58,41 +58,41 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `oobbit`.`connections` (
   `source_link_id` INT NOT NULL AUTO_INCREMENT,
   `destination_link_id` INT NOT NULL,
-  `title` VARCHAR(255) NULL DEFAULT NULL,
+  `title` VARCHAR(255) NULL,
   `creator` INT NOT NULL,
-  `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `edit_time` DATETIME NULL DEFAULT NULL,
+  `create_time` DATETIME NOT NULL,
+  `edit_time` DATETIME NULL,
   PRIMARY KEY (`source_link_id`, `destination_link_id`),
   INDEX `destination_link_id_idx` (`destination_link_id` ASC),
   CONSTRAINT `connection_source_link_id`
     FOREIGN KEY (`source_link_id`)
-    REFERENCES `oobbit`.`links` (`id`)
+    REFERENCES `oobbit`.`links` (`link_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `connection_destination_link_id`
     FOREIGN KEY (`destination_link_id`)
-    REFERENCES `oobbit`.`links` (`id`)
+    REFERENCES `oobbit`.`links` (`link_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `oobbit`.`comments` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `comment_id` INT NOT NULL AUTO_INCREMENT,
   `link_id` INT NOT NULL,
   `creator` INT NOT NULL,
   `content` MEDIUMTEXT NULL,
-  `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  `create_time` DATETIME NOT NULL,
+  PRIMARY KEY (`comment_id`),
   INDEX `link_id_idx` (`link_id` ASC),
   INDEX `creator_idx` (`creator` ASC),
   CONSTRAINT `comment_link_id`
     FOREIGN KEY (`link_id`)
-    REFERENCES `oobbit`.`links` (`id`)
+    REFERENCES `oobbit`.`links` (`link_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `comment_creator_id`
     FOREIGN KEY (`creator`)
-    REFERENCES `oobbit`.`users` (`id`)
+    REFERENCES `oobbit`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
